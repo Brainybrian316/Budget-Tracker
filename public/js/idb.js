cbffba9f64ed71fe4b3d10fb8c25a9bdc57ee931
Brainybrian316 <brainybrian316@gmail.com>
2022-06-24 15:41:04 -0400

    onsuccess and error function

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,5 +1,29 @@
-//*  variable to hold db connection
+//  variable to hold db connection
 let db; 
 
-// * we establish a connection to the db making a variable called request
+//  we establish a connection to the db making a variable called request
 const request = indexedDB.open("budget-tracker", 1);
+
+// an event to trigger when the db version changes
+request.onupgradeneeded = function(event) {
+  // saves a reference to the db
+  db = event.target.result;
+  // creates a new object store called pending with an autoIncrementing primary key
+  db.createObjectStore('pending', { autoIncrement: true });
+};
+
+//  if successful connection, then we trigger the following event
+request.onsuccess = function(event) {
+  // saves a reference to the db
+  db = event.target.result;
+  // if app is online upload pending transactions
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+// if there is an error, then we trigger the following event
+request.onerror = function(event) {
+  console.log(event.target.errorCode);
+}
+
