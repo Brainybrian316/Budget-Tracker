3db4dc2b323bb2464b2e12168c9b5b6e3c7ad245
Brainybrian316 <brainybrian316@gmail.com>
2022-06-24 17:11:16 -0400

    fetch event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -54,4 +54,43 @@ self.addEventListener('activate', event => {
 
 // fetch event listener
 self.addEventListener('fetch', event => {
-})
+  // if cache succeeds, then we trigger the GET request
+  if (event.request.url.includes('/api/') && event.request.method === 'GET') {
+    // we respond to the event with the cache
+  event.respondWith(
+    // we get the cache from the cache name
+    caches.open(CACHE_NAME)
+    // then we get the request from the cache name
+      .then(cache  => {
+        // return the cache as a fetch request
+        return fetch(event.request)
+        // if the request is successful, then we add the request to the cache using
+        .then(response => {
+          // if the response is valid, then we add it to the cache
+          if (response.status === 200) {
+            // we use .put to add the response to the cache and clone() to clone the response
+            cache.put(event.request, response.clone());
+          }
+          // return the response
+          return response;
+        })
+        // if the request is not successful, we catch the error and return the cache
+        .catch(() => {
+          // if failed, then we return the cache using .match to get the request from the cache that matches the request
+          return cache.match(event.request);
+        })
+      })
+      // we catch the error and return
+      .catch(err => console.log(err))
+  )
+  // stop executing the event
+  return;
+};
+    //  use offline fallback page if there is an error 
+    event.respondWith(
+      // we get the cache from the cache name that matches the request and we return the cache
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+});
