c7e7ca256bc65a65f8cbb89b9bea58eb82f75e14
Brainybrian316 <brainybrian316@gmail.com>
2022-06-24 10:51:40 -0400

    initial files

--- server.js
+++ server.js
@@ -16,14 +16,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI, 'mongodb://localhost:27017/budget-tracker', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+mongoose.set('debug', true);
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App connected to Budget Tracker on http://localhost:${PORT}/`);
 });
